d78f748a487836d85c9f4c5e7154c6afb5d9c7f2
Kevin Su <pingsutw@gmail.com>
2024-04-08 11:34:04 -0700

    Update functional tests
    Signed-off-by: Kevin Su <pingsutw@gmail.com>

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
 from flytekit import task, ImageSpec
-from flytekit.configuration import Config
+from flytekit.configuration import Config, SerializationSettings, ImageConfig
 from flytekit.remote import FlyteRemote
 from flytekit.tools.script_mode import hash_file
+
+remote = FlyteRemote(
+        config=Config.auto(),
+        default_project="flyte-conformance",
+        default_domain="development",
+    )
 
 image_spec = ImageSpec(
     name="flyte-conformance",
@@ -12,19 +18,14 @@ image_spec = ImageSpec(
 )
 
 
-@task(container_image=image_spec)
+@task(container_image=image_spec, cache=True, cache_version="1.0")
 def t1(x: int) -> datetime:
     return datetime.now()
 
 
 def test_cache_override():
-    # TODO: flytekit remote should upload workflow code to s3
     # TODO: update flytekit remote documentation. https://docs.flyte.org/en/latest/api/flytekit/design/control_plane.html#registering-entities
-    remote = FlyteRemote(
-        config=Config.auto(),
-        default_project="flytesnacks",
-        default_domain="development",
-    )
+
     _, digest, _ = hash_file(__file__)
     flyte_task = remote.register_task(
         entity=t1,
@@ -44,5 +45,22 @@ def test_cache_override():
     assert old != new
 
 
+def test_cache_output():
+    _, digest, _ = hash_file(__file__)
+    flyte_task = remote.register_task(
+        entity=t1,
+        version=digest,
+    )
+    exe = remote.execute(entity=flyte_task, inputs={"x": 2}, wait=True)
+    exe = remote.sync_execution(exe, sync_nodes=True)
+    old = exe.outputs["o0"]
+
+    exe = remote.execute(entity=flyte_task, inputs={"x": 2}, wait=True)
+    exe = remote.sync_execution(exe, sync_nodes=True)
+    new = exe.outputs["o0"]
+    assert old == new
+
+
 if __name__ == "__main__":
     test_cache_override()
+    test_cache_output()
