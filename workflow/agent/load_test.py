f060689bc9d2e5fa52384e46c5a9ad849901b56b
Kevin Su <pingsutw@apache.org>
2024-05-05 22:09:36 +0800

    Update launch plan (#4)
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- workflow/agent/load_test.py
+++ workflow/agent/load_test.py
@@ -2,7 +2,7 @@ from flytekit import workflow, task, LaunchPlan
 from flytekitplugins.dummy_agent import Sleep
 
 
-@task(task_config=Sleep(duration=2))
+@task(task_config=Sleep(duration=0.5))
 def sleep_task() -> str:
     return "Hello World!"
 
@@ -16,9 +16,9 @@ def sleep_wf():
 @workflow
 def load_test_wf():
     sleep_lp = LaunchPlan.get_or_create(
-        name="fixed_inputs", workflow=sleep_wf, max_parallelism=25
+        name="fixed_inputs", workflow=sleep_wf, max_parallelism=100
     )
-    for i in range(20):
+    for i in range(100):
         sleep_lp()
 
 
